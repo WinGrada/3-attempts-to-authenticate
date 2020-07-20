4ab91030a103e6bf61182fc37d01b96f8b97618b
Wingrada <wingrada@gmail.com>
2020-07-20 13:51:26 +0500

    Изменил функцию Generate Login. Добавил описание функцие.

--- 3 attempts to aunthenticate/Program.cs
+++ 3 attempts to aunthenticate/Program.cs
@@ -13,35 +13,52 @@ namespace _3_attempts_to_authenticate
 
 
         }
-
+        /* ===================================================================
+         *                  Генерация логина
+         * ===================================================================
+         * GenerateLogin
+         * 
+         * Генерирует логин, который имеет размер от 5 до 7 символов.
+         *  Возвращает строку логина.
+         * 
+         * Использует переменные:
+         *          allVowels - массив всех глассных англиского языка.
+         *          allConsonants - массив всех соглассных английского языка.
+        */
         static string GenerateLogin( char[] allVowels, char[] allConsonants)
         {
             Random random = new Random();
-            int rndNum = random.Next(5, 8);
-            int rndNumForVowels = default;
+            int rndSizeLogin = random.Next(5, 8);
+            int rndNum = default;
             bool rndBool = default;
-            char symbol = default;
             int cntVowels = 2;
 
             StringBuilder sb = new StringBuilder();
-            for (int i =  0; i < rndNum; i++)
+            for (int i =  0; i < rndSizeLogin; i++)
             {
+                //-----> Геренация первого символа в верхнем регистре.
                 if (i == 0)
                 {
-                    symbol = (char)random.Next(65, 91);
+                    char symbol = (char)random.Next(65, 91);
                     sb.Append(symbol);
                     continue;
                 }
 
+                //-----> Рандомное булевое значение.
                 rndBool = random.Next(2) == 0 ? false : true;
+
+                //-----> Генерация гласных символов в логине, не больше 2.
                 if (rndBool == true && cntVowels > 0)
                 {
-                    rndNumForVowels = random.Next(0, 6);
-                    sb.Append(allVowels[rndNumForVowels]);
+                    cntVowels--;
+                    rndNum = random.Next(0, 6);
+                    sb.Append(allVowels[rndNum]);
                     continue;
                 }
-                symbol = (char)random.Next(0, 21);
-                sb.Append(symbol);
+
+                //-----> Генерация согласных символов в логине.
+                rndNum = random.Next(0, 20);
+                sb.Append(allConsonants[rndNum]);
             }
             string result = sb.ToString();
 
