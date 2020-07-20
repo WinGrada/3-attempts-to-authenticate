559f8769bee4e5bb3a96ea3c363f0792c0c308ed
Wingrada <wingrada@gmail.com>
2020-07-20 10:42:58 +0500

    обновил часть функции

--- 3 attempts to aunthenticate/Program.cs
+++ 3 attempts to aunthenticate/Program.cs
@@ -7,28 +7,40 @@ namespace _3_attempts_to_authenticate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GenerateLogin());
+            char[] allVowels = new char[6] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            char[] allConsonants = new char[20] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z' };
+            Console.WriteLine(GenerateLogin(allVowels, allConsonants));
+
 
         }
 
-        static string GenerateLogin()
+        static string GenerateLogin( char[] allVowels, char[] allConsonants)
         {
             Random random = new Random();
             int rndNum = random.Next(5, 8);
-
-            //bool rndBool = random.Next(2) == 0 ? false : true;
-            //Console.WriteLine(rndBool);
+            int rndNumForVowels = default;
+            bool rndBool = default;
+            char symbol = default;
+            int cntVowels = 2;
 
             StringBuilder sb = new StringBuilder();
             for (int i =  0; i < rndNum; i++)
             {
                 if (i == 0)
                 {
-                    char firstSymbol = (char)random.Next(65, 91);
-                    sb.Append(firstSymbol);
+                    symbol = (char)random.Next(65, 91);
+                    sb.Append(symbol);
                     continue;
                 }
-                char symbol = (char)random.Next(97, 123);
+
+                rndBool = random.Next(2) == 0 ? false : true;
+                if (rndBool == true && cntVowels > 0)
+                {
+                    rndNumForVowels = random.Next(0, 6);
+                    sb.Append(allVowels[rndNumForVowels]);
+                    continue;
+                }
+                symbol = (char)random.Next(0, 21);
                 sb.Append(symbol);
             }
             string result = sb.ToString();
