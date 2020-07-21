dd5ec036136b4537bd86ac7be42368b983658d37
Wingrada <wingrada@gmail.com>
2020-07-21 11:41:28 +0500

    Закончил функцию генерацию пароль, и функцию объеденения логина и пароля. Добавил описание.

--- 3 attempts to aunthenticate/Program.cs
+++ 3 attempts to aunthenticate/Program.cs
@@ -10,6 +10,7 @@ namespace _3_attempts_to_authenticate
             char[] allVowels = new char[6] { 'a', 'e', 'i', 'o', 'u', 'y' };
             char[] allConsonants = new char[20] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z' };
             Console.WriteLine(GenerateLogin(allVowels, allConsonants));
+            Console.WriteLine(GeneratePassword());
 
 
         }
@@ -18,9 +19,16 @@ namespace _3_attempts_to_authenticate
          * ===================================================================
          * GenerateLogin
          * 
-         * Генерирует логин, который имеет размер от 6 до 9 символов.
-         *  Возвращает строку логина.
+         * С помощью String Builder -Генерирует логин, который имеет размер 
+         *  от 6 до 9 символов. Логин имеет 1 символ верхнего регистра, и в 
+         *  зависимости от рандома 0 или 2 гласных.
+         *  
+         *  Возвращает: строку состоящую из логина.
          * 
+         * Использует функции: 
+         *          GetRandomBoolValue - функция выдает рандомное булевое 
+         *           значение.
+         *           
          * Использует переменные:
          *          allVowels - массив всех глассных англиского языка.
          *          allConsonants - массив всех соглассных английского языка.
@@ -32,7 +40,7 @@ namespace _3_attempts_to_authenticate
             int rndNum = default;
             bool rndBool = default;
             int cntVowels = 2;
-
+            //-----> Дальше собирается строка с помощью конструктора.
             StringBuilder sb = new StringBuilder();
             for (int i =  0; i < rndSizeLogin; i++)
             {
@@ -63,15 +71,67 @@ namespace _3_attempts_to_authenticate
             return result;
         }
 
-       
-        //static char[] GeneratePassword()
-        //{
-
-        //}
-
-        static string GetUserLoginAndPassword()
+        /* ===================================================================
+         *                  Генерация пароля
+         * ===================================================================
+         * GeneratePassword
+         * 
+         * С помощью String Builder - Генерирует пароль от 6 символов, пароль 
+         *  состоит из букв вехнерго, нижнего регистра, так же имеются числа. 
+         *  Все это генерируется рандомно.
+         *  
+         * Возвращает: Строку состоящую из сгенерированого пароля.
+         *  
+         * Использует функции:
+         *          GetRandomBoolValue - функция выдает рандомное булевое 
+         *              значение.
+         *
+        */
+        static string GeneratePassword()
         {
-            return null;
+            char symbol = default;
+            bool rndBool = default;
+            Random random = new Random();
+            int recommendedSize = 4;
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < recommendedSize; i++)
+            {
+                //-----> Генерация символа верхнего регистра.
+                rndBool = GetRandomBoolValue();
+                if (rndBool)
+                {
+                    symbol = (char)random.Next(65, 91);
+                    sb.Append(symbol);
+                }
+
+                //-----> Генерация символа нижнего регистра.
+                rndBool = GetRandomBoolValue();
+                if (rndBool)
+                {
+                    symbol = (char)random.Next(97, 123);
+                    sb.Append(symbol);
+                }
+
+                //-----> Генерация чисел.
+                rndBool = GetRandomBoolValue();
+                if (rndBool)
+                {
+                    symbol = (char)random.Next(48, 58);
+                    sb.Append(symbol);
+                }
+            }
+            string result = sb.ToString();
+
+            return result;
+        }
+
+        static string CombineLoginAndPassword(char [] allVowels, char[] allConsonants)
+        {
+            string login = GenerateLogin(allVowels, allConsonants);
+            string password = GeneratePassword();
+
+            return $@"{login}\{password}";
         }
 
         static bool Сoincidence(string randomLoginAndPassword)
